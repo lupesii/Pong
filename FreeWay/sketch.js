ee823a250188bf02f13a0e753351bd06b903cbb8
Lucas Silva e Pereira <lucas4162007@gmail.com>
2023-03-10 20:01:33 -0300

    Add Sprites
    Adicionado os sprites da estrada, vaca e carro 1

--- FreeWay/sketch.js
+++ FreeWay/sketch.js
@@ -1,7 +1,25 @@
+//Imagens
+let estrada;
+let ator1;
+let carro1;
+
+function preload(){
+  estrada = loadImage("images/estrada.png");
+  ator1 = loadImage("images/ator-1.png");
+  carro1 = loadImage("images/carro-1.png");
+
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1400, 640);
 }
 
 function draw() {
-  background(220);
+  background(estrada);
+  showSprite();
+}
+
+function showSprite(){
+  image(ator1 ,350, 590, 40, 40);
+  image(carro1, 1400, 60, 70, 60)
 }

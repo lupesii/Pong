51d7c1047286a6861a183c79dd0807a1191d4c82
Lucas Silva e Pereira <lucas4162007@gmail.com>
2023-03-10 23:56:53 -0300

    Move Carro e Ator

--- FreeWay/sketch.js
+++ FreeWay/sketch.js
@@ -3,6 +3,12 @@ let estrada;
 let ator1;
 let carro1;
 
+//Ator
+yAtor = 590;
+
+//Carro
+xCar = 1400;
+
 function preload(){
   estrada = loadImage("images/estrada.png");
   ator1 = loadImage("images/ator-1.png");
@@ -17,9 +23,25 @@ function setup() {
 function draw() {
   background(estrada);
   showSprite();
+  moveAtor();
+  moveCarro();
 }
 
 function showSprite(){
-  image(ator1 ,350, 590, 40, 40);
-  image(carro1, 1400, 60, 70, 60)
+  image(ator1 ,350, yAtor, 40, 40);
+  image(carro1, xCar, 60, 70, 60)
+}
+
+function moveAtor(){
+  if (keyIsDown(UP_ARROW)){
+    yAtor -= 5
+  }
+  
+  if (keyIsDown(DOWN_ARROW)){
+    yAtor += 5
+  }
+}
+
+function moveCarro(){
+  xCar -= 4;
 }
